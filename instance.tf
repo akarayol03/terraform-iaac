dc45c00f51ccbad5f6d58283c0a26d9aec48330f
akarayol03 <akarayol03@gmail.com>
2019-12-16 23:53:35 -0600

    security group and fleet added

--- instance.tf
+++ instance.tf
@@ -3,9 +3,49 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro" 
   associate_public_ip_address = true
   key_name = aws_key_pair.deployer.key_name
-   user_data = file("userdata_file")
-
+  security_groups = "sg-17856248"
+  user_data = file("userdata_file")
+  
 tags = { 
-    Name = "HelloWorld" 
+    Name = "Elk1" 
+  } 
+}
+
+resource "aws_instance" "web2" { 
+  ami           = "ami-00068cd7555f543d5" 
+  instance_type = "t2.micro" 
+  associate_public_ip_address = true
+  key_name = aws_key_pair.deployer.key_name
+  security_groups = "sg-17856248"
+  user_data = file("userdata_file")
+  
+tags = { 
+    Name = "Elk2" 
+  } 
+}
+
+resource "aws_instance" "web3" { 
+  ami           = "ami-00068cd7555f543d5" 
+  instance_type = "t2.micro" 
+  associate_public_ip_address = true
+  key_name = aws_key_pair.deployer.key_name
+  security_groups = "sg-17856248"
+  user_data = file("userdata_file")
+  
+tags = { 
+    Name = "Elk3" 
+  } 
+}
+
+resource "aws_instance" "web4" { 
+  ami           = "ami-00068cd7555f543d5" 
+  instance_type = "t2.micro" 
+  associate_public_ip_address = true
+  key_name = aws_key_pair.deployer.key_name
+  security_groups = "sg-17856248"
+  user_data = file("userdata_file")
+  
+tags = { 
+    Name = "Elk4" 
   } 
 }
