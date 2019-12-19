c155ba90140781c2009a61d96b94b5850bdefe14
akarayol03 <akarayol03@gmail.com>
2019-12-18 20:09:00 -0600

    added10

--- output.tf
+++ output.tf
@@ -1,31 +1,28 @@
-#output "bucket_name" {
-#   value = aws_s3_bucket.b.bucket
-#}
+output "bucket_name" {
+    value = aws_s3_bucket.b.bucket
+}
 
-#output "sec_group" {
-#    value = aws_security_group.allow_ssh.name
-#}
+output "sec_group" {
+    value = aws_security_group.allow_ssh.name
+}
 
-#output "key_name" {
-#    value = aws_key_pair.deployer.key_name
-#}
+output "key_name" {
+    value = aws_key_pair.deployer.key_name
+}
 
-#output "username" {
-#    value = "some_username"
-#}
 
-#output "password" {
-#    value = "password"
-#}
 
-#output "wordpress" {
-#    value = aws_route53_record.www.name
-#}
+output "wordpress" {
+    value = aws_route53_record.www.name
+}
+output "wordpress_username" {
+    value = "some_username"
+}
+output "wordpress_password" {
+    value = "password"
+}
 
-#output "wordpress_username" {
-#    value = some_username
-#}
 
-#output "wordpress_password" {
-#    value = password
-#}
+output "IPs" {
+    value = aws_instance.web.*.public_ip
+}
