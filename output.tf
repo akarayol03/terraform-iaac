0a3f28ff1dc210c89bdc811fd1c6870ab96da113
akarayol03 <akarayol03@gmail.com>
2019-12-18 19:09:28 -0600

    output updated

--- output.tf
+++ output.tf
@@ -1,3 +1,19 @@
 output "bucket_name" {
     value = aws_s3_bucket.b.bucket
 }
+
+output "sec_group" {
+    value = aws_security_group.allow_ssh.name
+}
+
+output "key_name" {
+    value = aws_key_pair.deployer.keey_name
+}
+
+output "username" {
+    value = "some_username"
+}
+
+output "password" {
+    value = "password"
+}
