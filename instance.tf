e9edd21e4c66aada671cc99fd77db21843ffd5fb
akarayol03 <akarayol03@gmail.com>
2019-12-21 10:48:47 -0600

    added

--- instance.tf
+++ instance.tf
@@ -1,15 +1,14 @@
 resource "aws_instance" "web" { 
   ami           = "ami-00068cd7555f543d5" 
+  count = 10
   instance_type = "t2.micro" 
   associate_public_ip_address = "true"
   key_name = aws_key_pair.deployer.key_name
   security_groups = ["allow_ssh"]
   root_block_device {
       volume_size = 25
-  }
   user_data = file("userdata_file")
-  
-tags = { 
+  tags = { 
     Name = "HelloWorld"
   } 
 }
