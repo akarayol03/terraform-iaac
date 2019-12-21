738010e34e63e22a004f53c618684b9f6de994ce
akarayol03 <akarayol03@gmail.com>
2019-12-21 10:51:57 -0600

    added

--- instance.tf
+++ instance.tf
@@ -1,15 +1,22 @@
-resource "aws_instance" "web" { 
-  ami           = "ami-00068cd7555f543d5" 
+resource "aws_instance" "web" {
   count = 10
-  instance_type = "t2.micro" 
+  ami             = "ami-00068cd7555f543d5"
+  instance_type   = "t2.micro"
   associate_public_ip_address = "true"
   key_name = aws_key_pair.deployer.key_name
   security_groups = ["allow_ssh"]
-  root_block_device {
-      volume_size = 25
   user_data = file("userdata_file")
-  tags = { 
-    Name = "HelloWorld"
-  } 
+  lifecycle{
+    prevent_destroy = false
+  }
+  tags = {
+    Name = "HelloWorld${count.index +1}"
+  }
+}
+resource "aws_instance" "imported" {
+  ami             = "ami-00068cd7555f543d5"
+  key_name        = aws_key_pair.deployer.key_name
+  security_groups = ["allow_ssh"]
+  instance_type  = "t2.micro"
 }
 
